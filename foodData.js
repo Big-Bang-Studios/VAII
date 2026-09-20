1efa4f2c7eea70e24d81784d67fee54db1cbb52b
loganbaezmelo-dot <loganbaezmelo@gmail.com>
2026-09-20 13:48:29 +0000

    properly separate foodData.js and restore script.js

--- foodData.js
+++ foodData.js
@@ -1,1 +1,191 @@
 export const LOCAL_FOOD_DB = {
+    "burger": [
+        { name: "Wendy's", item: "Baconator with fries" },
+        { name: "Five Guys", item: "Bacon Cheeseburger with Cajun Fries" },
+        { name: "Shake Shack", item: "ShackBurger and a milkshake" },
+        { name: "In-N-Out Burger", item: "Double-Double Animal Style" },
+        { name: "McDonald's", item: "Big Mac meal" },
+        { name: "Burger King", item: "Whopper with cheese" },
+        { name: "Smashburger", item: "Classic Double Smash" },
+        { name: "Whataburger", item: "Patty Melt" },
+        { name: "Carl's Jr", item: "Western Bacon Cheeseburger" },
+        { name: "Hardee's", item: "Monster Thickburger" },
+        { name: "Culver's", item: "ButterBurger Cheese" },
+        { name: "Steak 'n Shake", item: "Original Double 'n Cheese" },
+        { name: "Jack in the Box", item: "Ultimate Cheeseburger" },
+        { name: "Dairy Queen", item: "FlameThrower GrillBurger" },
+        { name: "Sonic Drive-In", item: "SuperSONIC Double Cheeseburger" }
+    ],
+    "fries": [
+        { name: "Five Guys", item: "Large Cajun Fries" },
+        { name: "McDonald's", item: "Large World Famous Fries" },
+        { name: "Wendy's", item: "Baconator Fries" },
+        { name: "Checkers", item: "Fully Loaded Fries" },
+        { name: "Rally's", item: "Famous Seasoned Fries" },
+        { name: "Arby's", item: "Curly Fries" },
+        { name: "Chick-fil-A", item: "Waffle Potato Fries" },
+        { name: "Shake Shack", item: "Cheese Fries" },
+        { name: "Steak 'n Shake", item: "Cheese 'n Bacon Fries" },
+        { name: "Taco Bell", item: "Nacho Fries" }
+    ],
+    "pizza": [
+        { name: "Domino's Pizza", item: "ExtravaganZZa Specialty Pizza" },
+        { name: "Pizza Hut", item: "Meat Lover's Pan Pizza" },
+        { name: "Papa John's", item: "The Works Pizza" },
+        { name: "Little Caesars", item: "Hot-N-Ready Pepperoni" },
+        { name: "Papa Murphy's", item: "Cowboy Take 'N' Bake" },
+        { name: "Marco's Pizza", item: "Pepperoni Magnifico" },
+        { name: "Jet's Pizza", item: "Detroit-Style Deep Dish" },
+        { name: "Blaze Pizza", item: "Build Your Own Artisanal Pizza" },
+        { name: "MOD Pizza", item: "Mad Dog Pizza" },
+        { name: "California Pizza Kitchen", item: "Original BBQ Chicken Pizza" },
+        { name: "Mellow Mushroom", item: "Holy Shiitake Pie" },
+        { name: "Cici's", item: "Mac & Cheese Pizza" },
+        { name: "Sbarro", item: "XL NY Style Pepperoni Slice" },
+        { name: "Hungry Howie's", item: "Flavored Crust Pizza" },
+        { name: "Godfather's Pizza", item: "Classic Combo" }
+    ],
+    "chicken": [
+        { name: "Chick-fil-A", item: "Spicy Chicken Sandwich" },
+        { name: "Popeyes", item: "Classic Chicken Sandwich" },
+        { name: "KFC", item: "Famous Bowl" },
+        { name: "Church's Chicken", item: "Spicy Bone-In Chicken" },
+        { name: "Bojangles", item: "Cajun Chicken Filet Biscuit" },
+        { name: "Zaxby's", item: "Chicken Finger Plate" },
+        { name: "Raising Cane's", item: "The Box Combo" },
+        { name: "El Pollo Loco", item: "Fire-Grilled Chicken Meal" },
+        { name: "Wingstop", item: "Lemon Pepper Wings" },
+        { name: "Buffalo Wild Wings", item: "Honey BBQ Boneless Wings" },
+        { name: "Jollibee", item: "Chickenjoy" },
+        { name: "PDQ", item: "Crispy Chicken Tenders" }
+    ],
+    "mexican": [
+        { name: "Taco Bell", item: "Crunchwrap Supreme" },
+        { name: "Chipotle", item: "Steak Burrito Bowl" },
+        { name: "Qdoba", item: "3-Cheese Queso Burrito" },
+        { name: "Moe's Southwest Grill", item: "Homewrecker Burrito" },
+        { name: "Del Taco", item: "The Del Taco" },
+        { name: "Baja Fresh", item: "Baja Burrito" },
+        { name: "Taco John's", item: "Potato Olés" },
+        { name: "Torchy's Tacos", item: "Trailer Park Taco" },
+        { name: "On The Border", item: "Fajitas" },
+        { name: "Chuy's", item: "Chick-a-Chuy Chimi" },
+        { name: "Fuzzy's Taco Shop", item: "Baja Tacos" }
+    ],
+    "sandwich": [
+        { name: "Subway", item: "Italian B.M.T." },
+        { name: "Jersey Mike's", item: "Original Italian" },
+        { name: "Jimmy John's", item: "Vito Sub" },
+        { name: "Firehouse Subs", item: "Hook & Ladder" },
+        { name: "Panera Bread", item: "Bacon Turkey Bravo" },
+        { name: "Quiznos", item: "Classic Italian" },
+        { name: "Potbelly", item: "A Wreck Sandwich" },
+        { name: "Schlotzsky's", item: "The Original" },
+        { name: "Jason's Deli", item: "Muffaletta" },
+        { name: "McAlister's Deli", item: "McAlister's Club" },
+        { name: "Arby's", item: "Classic Roast Beef" },
+        { name: "Penn Station", item: "Philly Cheesesteak" },
+        { name: "Which Wich", item: "The Wicked" }
+    ],
+    "coffee": [
+        { name: "Starbucks", item: "Caramel Macchiato" },
+        { name: "Dunkin'", item: "Iced Coffee with Hazelnut" },
+        { name: "Peet's Coffee", item: "Major Dickason's Blend" },
+        { name: "Dutch Bros", item: "Golden Eagle" },
+        { name: "Caribou Coffee", item: "Campfire Mocha" },
+        { name: "Tim Hortons", item: "Iced Capp" },
+        { name: "The Coffee Bean", item: "Ice Blended Drink" },
+        { name: "Biggby Coffee", item: "Caramel Marvel" },
+        { name: "Scooter's Coffee", item: "Caramelicious" },
+        { name: "Philz Coffee", item: "Mint Mojito Iced Coffee" }
+    ],
+    "ice cream": [
+        { name: "Baskin-Robbins", item: "Mint Chocolate Chip" },
+        { name: "Dairy Queen", item: "Oreo Blizzard" },
+        { name: "Cold Stone Creamery", item: "Founder's Favorite" },
+        { name: "Ben & Jerry's", item: "Half Baked" },
+        { name: "Haagen-Dazs", item: "Dulce de Leche Dazzler" },
+        { name: "Rita's", item: "Mango Gelati" },
+        { name: "Culver's", item: "Turtle Sundae" },
+        { name: "Braum's", item: "Premium Ice Cream Cone" },
+        { name: "Bruster's", item: "Waffle Cone" },
+        { name: "Marble Slab", item: "Sweet Cream with Mix-ins" }
+    ],
+    "donuts": [
+        { name: "Dunkin'", item: "Boston Kreme Donut" },
+        { name: "Krispy Kreme", item: "Original Glazed" },
+        { name: "Tim Hortons", item: "Timbits" },
+        { name: "Shipley Do-Nuts", item: "Glazed Do-Nut" },
+        { name: "Voodoo Doughnut", item: "Bacon Maple Bar" },
+        { name: "Duck Donuts", item: "Bacon in the Sun" },
+        { name: "Stan's Donuts", item: "Biscoff Banana Pocket" }
+    ],
+    "breakfast": [
+        { name: "IHOP", item: "Rooty Tooty Fresh 'N Fruity" },
+        { name: "Denny's", item: "Grand Slam" },
+        { name: "Waffle House", item: "All-Star Special" },
+        { name: "Cracker Barrel", item: "Momma's Pancake Breakfast" },
+        { name: "First Watch", item: "Million Dollar Bacon" },
+        { name: "Bob Evans", item: "Farmer's Choice" },
+        { name: "Village Inn", item: "Lumberjack Breakfast" },
+        { name: "Perkins", item: "Tremendous Twelve" },
+        { name: "Snooze", item: "Pineapple Upside Down Pancakes" },
+        { name: "The Original Pancake House", item: "Apple Pancake" }
+    ],
+    "asian": [
+        { name: "Panda Express", item: "Orange Chicken with Chow Mein" },
+        { name: "P.F. Chang's", item: "Lettuce Wraps" },
+        { name: "Pei Wei", item: "Kung Pao Chicken" },
+        { name: "Yoshinoya", item: "Gyudon Beef Bowl" },
+        { name: "Sarku Japan", item: "Chicken Teriyaki" },
+        { name: "Genghis Grill", item: "Build Your Own Bowl" },
+        { name: "Kona Grill", item: "Macadamia Nut Chicken" },
+        { name: "Benihana", item: "Hibachi Steak" },
+        { name: "Sushi-San", item: "Spicy Tuna Roll" },
+        { name: "Nobu", item: "Black Cod Miso" }
+    ],
+    "seafood": [
+        { name: "Red Lobster", item: "Cheddar Bay Biscuits & Shrimp" },
+        { name: "Long John Silver's", item: "Fish & Chicken Platter" },
+        { name: "Captain D's", item: "Deluxe Seafood Platter" },
+        { name: "Bonefish Grill", item: "Bang Bang Shrimp" },
+        { name: "Joe's Crab Shack", item: "Crab Bucket" },
+        { name: "Bubba Gump Shrimp", item: "Dumb Luck Coconut Shrimp" },
+        { name: "McCormick & Schmick's", item: "Fresh Catch" },
+        { name: "Legal Sea Foods", item: "New England Clam Chowder" }
+    ],
+    "steakhouse": [
+        { name: "Outback Steakhouse", item: "Bloomin' Onion & Sirloin" },
+        { name: "Texas Roadhouse", item: "Bone-In Ribeye with Rolls" },
+        { name: "LongHorn Steakhouse", item: "Flo's Filet" },
+        { name: "Ruth's Chris", item: "Petite Filet" },
+        { name: "Capital Grille", item: "Dry Aged NY Strip" },
+        { name: "Fogo de Chao", item: "Full Churrasco Experience" },
+        { name: "Black Angus", item: "Campfire Feast" },
+        { name: "Morton's", item: "Center-Cut Filet Mignon" },
+        { name: "Saltgrass", item: "Pat's Ribeye" },
+        { name: "Fleming's", item: "Prime Tomahawk" }
+    ],
+    "italian": [
+        { name: "Olive Garden", item: "Tour of Italy" },
+        { name: "Carrabba's", item: "Chicken Bryan" },
+        { name: "Maggiano's", item: "Rigatoni D" },
+        { name: "Macaroni Grill", item: "Penne Rustica" },
+        { name: "Fazoli's", item: "Baked Ziti" },
+        { name: "Buca di Beppo", item: "Spaghetti with Meatballs" },
+        { name: "Old Spaghetti Factory", item: "Mizithra Cheese & Browned Butter" },
+        { name: "Carbone", item: "Spicy Rigatoni Vodka" }
+    ]
+};
+
+export const ALL_FOOD_SUGGESTIONS = [];
+
+Object.keys(LOCAL_FOOD_DB).forEach(cat => {
+    ALL_FOOD_SUGGESTIONS.push(`Order ${cat}`);
+    ALL_FOOD_SUGGESTIONS.push(`Reserve ${cat}`);
+});
+
+Object.values(LOCAL_FOOD_DB).flat().forEach(b => {
+    ALL_FOOD_SUGGESTIONS.push(`Order from ${b.name}`);
+    ALL_FOOD_SUGGESTIONS.push(`Reserve table at ${b.name}`);
+});
